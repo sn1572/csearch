59209ccd8c4b048638639ed5a3c339f33275660c
mark <mark.bolding@gtri.gatech.edu>
2021-02-04 15:39:44 -0500

    Added recursive mode

--- csearch.py
+++ csearch.py
@@ -54,37 +54,39 @@ def _is_object_file(fname):
 	return(_check_regex(fname, r))
 
 
-def _search_directory(dname, token, type_check_fun, search_fun):
+def _search_directory(dname, token, type_check_fun, search_fun, r):
 	contents = os.listdir(dname)
 	for thing in contents:
 		thing = os.path.join(dname, thing)
 		if os.path.isfile(thing):
 			if type_check_fun(thing):
 				search_fun(thing, token)
+		elif (r and os.path.isdir(thing)):
+			_search_directory(thing, token, type_check_fun, search_fun, r)
 
 
-def main(name, token, type_check_fun, search_fun):
+def main(name, token, type_check_fun, search_fun, r):
 	if os.path.isfile(name):
 		if type_check_fun(name):
 			search_fun(name, token)
 		else:
 			raise TypeError("Incorrect file type.")
 	elif os.path.isdir(name):
-		_search_directory(name, token, type_check_fun, search_fun)
+		_search_directory(name, token, type_check_fun, search_fun, r)
 	else:
 		raise TypeError("Must be a directory or file.")
 
 
-def object_main(name, token):
-	main(name, token, _is_object_file, _nm_search)
+def object_main(name, token, r):
+	main(name, token, _is_object_file, _nm_search, r)
 
 
-def source_main(name, token):
-	main(name, token, _is_c_source, _cat_search)
+def source_main(name, token, r):
+	main(name, token, _is_c_source, _cat_search, r)
 
 
-def header_main(name, token):
-	main(name, token, _is_c_header, _cat_search)
+def header_main(name, token, r):
+	main(name, token, _is_c_header, _cat_search, r)
 
 
 if __name__ == '__main__':
@@ -95,6 +97,10 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		allow_abbrev=True,
 		description=descr)
+	parser.add_argumen(('--recursive', '-r', '-R',
+		action='store_true',
+		default=False,
+		help="Recursive mode. Causes subdirectories to be searched.")
 	parser.add_argument('--object', '-o',
 		action='store_true',
 		default=False,
@@ -123,12 +129,13 @@ if __name__ == '__main__':
 	o_mode = args.object
 	s_mode = args.source
 	h_mode = args.header
+	r = args.recursive
 	if not (o_mode or s_mode or h_mode):
 		raise Exception("Must select a mode of operation (-s or -o)")
 
 	if o_mode:
-		object_main(directory, symbol)
+		object_main(directory, symbol, r)
 	if s_mode:
-		source_main(directory, symbol)
+		source_main(directory, symbol, r)
 	if h_mode:
-		header_main(directory, symbol)
+		header_main(directory, symbol, r)
